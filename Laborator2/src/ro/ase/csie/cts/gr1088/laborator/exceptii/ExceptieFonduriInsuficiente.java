844f53d28b17496ce726d350fe24681d6474b235
sorinaalexandraserban <sorinaserban6582@gmail.com>
2021-03-04 12:23:27 +0200

    laborator 2

---
+++ Laborator2/src/ro/ase/csie/cts/gr1088/laborator/exceptii/ExceptieFonduriInsuficiente.java
@@ -0,0 +1,12 @@
+package ro.ase.csie.cts.gr1088.laborator.exceptii;
+
+public class ExceptieFonduriInsuficiente extends Exception {
+	
+	public ExceptieFonduriInsuficiente(String mesaj) {
+		super(mesaj);
+	}
+	
+	public ExceptieFonduriInsuficiente() {
+		
+	}
+}
---
+++ Laborator2/src/ro/ase/csie/cts/gr1088/laborator/exceptii/ExceptieTransferIlegal.java
@@ -0,0 +1,12 @@
+package ro.ase.csie.cts.gr1088.laborator.exceptii;
+
+public class ExceptieTransferIlegal extends Exception {
+
+	public ExceptieTransferIlegal(String mesaj) {
+		super(mesaj);
+	}
+	
+	public ExceptieTransferIlegal() {
+		
+	}
+}
