f82df032ef74bc5bdd5e67ef6af0fe382a18d895
sorinaalexandraserban <sorinaserban6582@gmail.com>
2021-03-04 13:33:28 +0200

    seminar 2 final

---
+++ Laborator2/src/ro/ase/csie/cts/gr1088/laborator/modele/ContBancar.java
@@ -0,0 +1,17 @@
+package ro.ase.csie.cts.gr1088.laborator.modele;
+
+public abstract class ContBancar extends Cont {
+
+	protected double balanta;
+	protected String iban;
+
+	public ContBancar(double balanta, String iban) {
+		this.balanta = balanta;
+		this.iban = iban;
+	}
+
+	@Override
+	public double getBalanta() {
+		return this.balanta;
+	}
+}
