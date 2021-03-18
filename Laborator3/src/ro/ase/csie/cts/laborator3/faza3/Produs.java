0c56e52c083c2377ec3c6c8dad98eb5a57727743
sorinaalexandraserban <sorinaserban6582@gmail.com>
2021-03-18 12:23:16 +0200

    commit la pauza

---
+++ Laborator3/src/ro/ase/csie/cts/laborator3/faza3/Produs.java
@@ -0,0 +1,29 @@
+package ro.ase.csie.cts.laborator3.faza3;
+
+import ro.ase.csie.cts.laborator3.exceptii.ExceptiePretInvalid;
+import ro.ase.csie.cts.laborator3.exceptii.ExceptieVechimeClient;
+import ro.ase.csie.cts.laborator3.faza3.servicii.InterfataMarketing;
+import ro.ase.csie.cts.laborator3.faza3.servicii.InterfataValidare;
+
+public class Produs {
+	
+	InterfataMarketing serviciuMarketing = null;
+	InterfataValidare serviciuValidare = null;
+	
+	public static float getPretCuDiscount(float pretInitial, float discount) {
+		return pretInitial - (discount * pretInitial);
+	}
+	
+	public float getPretFinal(TipProdus tipProdus, float pretInitial, int vechimeClientInAni) throws ExceptieVechimeClient, ExceptiePretInvalid
+	{
+		serviciuValidare.validarePret(pretInitial);
+		serviciuValidare.validareVechimeClient(vechimeClientInAni);
+	    
+	    float discountFidelitate = (tipProdus == TipProdus.NOU) ? 0 : serviciuMarketing.getDiscountFidelitate(vechimeClientInAni);
+	    float discount = tipProdus.getDiscount();
+		float valoareDiscountTipProdus = getPretCuDiscount(pretInitial, discount);
+        float pretFinal = valoareDiscountTipProdus * ( 1- discountFidelitate );
+	    
+	    return pretFinal;
+	  }
+}
