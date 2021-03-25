31039cc2781af3d6c4f9ccf3ef14aa96c220a7a4
sorinaalexandraserban <sorinaserban6582@gmail.com>
2021-03-25 13:25:03 +0200

    exemplu Prototype + Registru Singleton

---
+++ Laborator5 - Singleton si Prototype/src/ro/ase/csie/cts/dp/singleton/registru/ConexiuneBD.java
@@ -0,0 +1,25 @@
+package ro.ase.csie.cts.dp.singleton.registru;
+
+import java.util.Hashtable;
+
+public class ConexiuneBD {
+
+	String ip;
+	String denumire;
+	
+	private static Hashtable<String, ConexiuneBD> conexiuni = new Hashtable<>();
+
+	private ConexiuneBD(String ip, String denumire) {
+		this.ip = ip;
+		this.denumire = denumire;
+	}
+	
+	public static ConexiuneBD getConexiune(String ip, String denumire) {
+		ConexiuneBD conexiune = conexiuni.get(ip);
+		if(conexiune == null) {
+			conexiune = new ConexiuneBD(ip,denumire);
+			conexiuni.put(ip,conexiune);
+		}
+		return conexiune;
+	}
+}
